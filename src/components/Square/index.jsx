fa12e2a3d64d1a68560911972f5bc5cd53526c84
Gustavo <gustavonovais@febracis.com>
2021-01-20 18:06:13 -0300

    first commit

---
+++ src/components/Square/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css'
+
+class Square extends React.Component {
+  render() {
+    return(
+      <button className='square'>
+        {/* TO DO */}
+      </button>
+    );
+  }
+}
+
+export default Square;
